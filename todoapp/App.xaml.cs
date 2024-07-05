471aaccae65eb3ffb7df7d60eda10a3e658f1fbc
krystian.rozwag <krystek_1@onet.eu>
2024-07-05 15:08:00 +0200

    first todo boilerplate

---
+++ todoapp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace todoapp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
